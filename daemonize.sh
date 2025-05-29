da69af111f66fb245741e205f61d73b423551660
horrifyingHorse <excuseplanation@gmail.com>
2025-05-30 00:23:05 +0530

    daemon: finally a working daemon

--- daemonize.sh
+++ daemonize.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 FILE=haka.service
-LINE=0
+HAKA=haka.out
 
 write() {
   echo $@ >>$FILE
@@ -10,11 +10,16 @@ write() {
 checkfile() {
   if [[ -f $FILE ]]; then
     echo "$FILE already exists. use -f to force"
+    exit 1
   fi
 }
 
 openfile() {
   rm -rf $FILE
+}
+
+silent() {
+  $@ >/dev/null 2>&1
 }
 
 if [[ "$1" != "-f" ]]; then
@@ -24,14 +29,29 @@ openfile
 
 write [Unit]
 write Description=Haka Keyboard Listener Daemon
-write After=network.target
 write
 write [Service]
 write Type=simple
-write ExecStart=$(pwd)/haka.out
+write ExecStart=$(pwd)/$HAKA
 write WorkingDircetory=$(pwd)
-write User=$(whoami)
+# Do not need the User field
+# it is only needed when the daemon is running as a root
+# daemon. For a --user daemon, specifying User field
+# causes it to break severely
+# write User=$(whoami)
 write Restart=on-failure
+write StandardOutput=journal
+write StandardError=journal
 write
 write [Install]
 write WantedBy=deault.target
+
+silent systemctl --user status $FILE
+silent systemctl --user disable $FILE
+ln -s $(pwd)/$FILE ~/.config/systemd/user/
+systemctl --user daemon-reload
+systemctl --user enable --now $FILE
+systemctl --user status $FILE
+
+# To check logs:
+# $ journalctl --user -u $FILE -f
